4324e2320f2ac27d0914bb27154004f872679c7d
Evan McKinney-Larkin <evanmckinney07@gmail.com>
2021-02-01 21:51:49 -0600

    world series

--- dictionary excercise 2.py
+++ dictionary excercise 2.py
@@ -1,14 +1,29 @@
-winners = open("WorldSeriesWinners.txt", "r")
-
+winnersList = open("WorldSeriesWinners.txt").read().splitlines()
 
 teamWinners = {}
+winFrequency = {}
 
-# userYear = int(input("Input the year of the team that won: "))
+userYear = int(input("Input the year of the team that won (1903 - 2009): "))
 count = 1903
-for team in winners:
-    teamWinners[team] = count
-    count +=1 
-    print(teamWinners)
+teamCount = 0
 
+
+for team in winnersList:
+
+    # if count != 1904 or count != 1994:
+
+    winFrequency[team] = winnersList.count(team)
+    count += 1
+    
+    teamWinners[count] = winnersList[teamCount]
+    teamCount += 1
     
     
+
+teamName = teamWinners.get(userYear)
+print(teamWinners.get(userYear))
+print(winFrequency.get(teamName))
+
+
+# print(teamWinners)
+# print(winFrequency)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+while count != 2010:
+#     teamWinners[team] = count
+#     count += 1
+#     teamCount+=1
