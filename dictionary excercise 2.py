b64cfcc6b14783f7f01ea16e5f8b22206a7f2d7d
Evan McKinney-Larkin <evanmckinney07@gmail.com>
2021-02-05 21:32:50 -0600

    team winners dictionary updated

--- dictionary excercise 2.py
+++ dictionary excercise 2.py
@@ -5,28 +5,41 @@ winFrequency = {}
 
 count = 1903
 teamCount = 0
-userYear = int(input("Input the year of the team that won (1903 - 2009): "))
+userYear = int(input("Input the year of the team that won (1903 - 2008): "))
 
 
 for team in winnersList:
-    
-    if userYear == 1904 or userYear == 1994:
-        print("The world series did not happen that year")
-        break
+
+    if count == 1904:
+        count += 1
+        teamWinners[1905] = 'New York Giants'
+
+    elif count == 1994:
+        count+=1
+        teamWinners[1995] = 'Atlanta Braves'
     else:
-        
+
         winFrequency[team] = winnersList.count(team)
-        
 
         teamWinners[count] = winnersList[teamCount]
-        teamCount += 1
-        count += 1
 
-     
+    count += 1
+
+    teamCount += 1
+
+
+print(teamWinners)
+
+
 teamName = teamWinners.get(userYear)
-        
-print("The",teamWinners.get(userYear), "won the World Series", winFrequency.get(teamName), "time(s)")
 
-
-
-
+if userYear == 1904 or userYear == 1994:
+    print("The World Series was not played that year")
+else:
+    print(
+        "The",
+        teamWinners.get(userYear),
+        "won the World Series that year for a total of",
+        winFrequency.get(teamName),
+        "time(s)",
+    )
